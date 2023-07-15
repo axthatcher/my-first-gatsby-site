e964298c8e97e416af4d61eb5a36c0d9fd954cd5
axthatcher <axthatcher@gmail.com>
2023-07-15 15:40:20 -0600

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,18 @@
  */
 module.exports = {
   siteMetadata: {
+    title: "Me gustan los tacos",
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
     "gatsby-plugin-image",
-    "gatsby-plugin-sharp"],
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+  ],
 }
